7874638c630cba620b718b017ee9ae68bcf20b2d
Anatoli Huseu <avg.tolik@gmail.com>
2020-02-04 11:37:26 +0300

    feat: add options for git-tag command

--- .bin/clier.js
+++ .bin/clier.js
@@ -8,7 +8,9 @@ cli.version(version);
 
 // GIT TAG Command
 cli
-  .command('git-tag <name>')
+  .command('git-tag')
+  .option('-n, --name <tag-name>', 'Combine tag name from `name` and timestamp')
+  .option('-v, --version <tag-version>', 'Use version of tag name')
   .description('Create new git tag with given name and push it to origin')
   .action(commands.gitTag);
 
--- src/commands/git-tag.js
+++ src/commands/git-tag.js
@@ -1,6 +1,6 @@
 const { execCommands } = require('../utils');
 
-const gitTag = (name) => {
+const createTagInfoWithTimestamp = (name) => {
   const date = new Date().toISOString().replace(/\.\d{3}/g, '');
 
   const dateForName = date.replace('T', '__').replace(/:/g, '-'); // 2020-02-04__00-00-00Z
@@ -8,6 +8,32 @@ const gitTag = (name) => {
 
   const tagName = `${name}--${dateForName}`;
   const tagMessage = `Release: ${name} at ${dateForMessage}`;
+
+  return { tagName, tagMessage };
+};
+
+const createTagInfoWithVersion = (version) => {
+  const tagName = `v${version}`;
+  const tagMessage = `Release ${tagName}`;
+
+  return { tagName, tagMessage };
+};
+
+const createTagInfo = (cmdObj) => {
+  if (typeof cmdObj.name === 'string') {
+    return createTagInfoWithTimestamp(cmdObj.name);
+  }
+
+  if (typeof cmdObj.version === 'string') {
+    return createTagInfoWithVersion(cmdObj.version);
+  }
+
+  console.log('💥 Select any option');
+  process.exit(0);
+};
+
+const gitTag = (cmdObj) => {
+  const { tagName, tagMessage } = createTagInfo(cmdObj);
 
   execCommands([`git tag -a ${tagName} -m "${tagMessage}"`, `git push origin ${tagName}`]);
 };
